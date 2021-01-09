21f118a308f02ace55fa9731849d3f81c168ff68
Sawyer <sawyercoletang@gmail.com>
2021-01-09 18:01:23 -0500

    added openCV dependency to pom.xml

---
+++ src/main/java/openCV/CoinFinder.java
@@ -0,0 +1,17 @@
+package openCV;
+
+import org.opencv.core.Mat;
+
+import java.util.ArrayList;
+
+public class CoinFinder {
+
+    public ArrayList<Integer> findCoins(){
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        return list;
+
+    }
+
+}
