5bb8d4b9315eb27dc133eb858b42197d198d1c46
Sawyer <sawyercoletang@gmail.com>
2021-01-10 10:37:49 -0500

    getting closer to figuring out the circles

--- src/main/java/openCV/CoinFinder.java
+++ src/main/java/openCV/CoinFinder.java
@@ -5,7 +5,6 @@ import nu.pattern.OpenCV;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
-import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
@@ -13,7 +12,6 @@ import java.util.ArrayList;
 
 import static org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_COLOR;
 import static org.opencv.imgproc.Imgproc.COLOR_RGB2GRAY;
-import static org.opencv.core.CvType.CV_8U;
 import static org.opencv.imgproc.Imgproc.CV_HOUGH_GRADIENT;
 
 
@@ -26,14 +24,15 @@ public class CoinFinder {
         String file = "src/main/resources/image1.jpg";
         Mat src = Imgcodecs.imread(file, CV_LOAD_IMAGE_COLOR);
 
-        System.out.println(findCoins2(src).toString());
+        System.out.println(findCoins(src).toString());
 
 
     }
 
-    public static ArrayList<Integer> findCoins2(Mat mat) throws InterruptedException {
+    public static ArrayList<Integer> findCoins(Mat mat) throws InterruptedException {
         ArrayList<Integer> list = new ArrayList<Integer>();
 
+        //TODO: collapse code... only temporarily chunky for optimization
 
         Mat blur = new Mat();
         Mat edge = new Mat();
@@ -42,9 +41,10 @@ public class CoinFinder {
         Imgproc.cvtColor(mat, mat, COLOR_RGB2GRAY);
         Imgcodecs.imwrite("gray.jpg", mat);
 
-        Imgproc.blur(mat, blur, new Size(3,3));
+        Imgproc.blur(mat, blur, new Size(5,5));
+        Imgcodecs.imwrite("blur.jpg", blur);
 
-        int threshold = 30;
+        int threshold = 10;
 
         Imgproc.Canny(blur, edge,threshold, threshold*3, 3, true);
         Imgcodecs.imwrite("edge.jpg", edge);
@@ -56,7 +56,10 @@ public class CoinFinder {
         Mat circles = new Mat();
 
 
-        Imgproc.HoughCircles(blur2, circles, CV_HOUGH_GRADIENT, 1, blur2.rows() / 8,20, 100, 0, 0);
+        //TODO: make minRadius more robust using , not a hardcoded value
+        Imgproc.HoughCircles(blur2, circles, CV_HOUGH_GRADIENT, 1, blur2.rows() / 8,20, 100, 0, blur2.rows() / 10);
+
+        System.out.println(blur2.rows());
 
         for (int x = 0; x < circles.cols(); x++) {
             double[] c = circles.get(0, x);
@@ -72,62 +75,6 @@ public class CoinFinder {
         Imgcodecs.imwrite("blur2.jpg", blur2);
 
         return list;
-    }
-
-    public static ArrayList<Integer> findCoins(Mat mat) throws InterruptedException {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-
-
-        //TODO: collapse code... only temporarily chunky for optimization
-        Mat gray = new Mat();
-        Imgproc.cvtColor(mat, gray, COLOR_RGB2GRAY);
-        Imgcodecs.imwrite("gray.jpg", gray);
-
-        Mat edge = new Mat();
-        Imgproc.Canny(gray, edge,30, 130, 3, true);
-//        Imgproc.cvtColor(gray, edge, CV_8U);
-        Imgcodecs.imwrite("edge.jpg", edge);
-
-        Mat draw = new Mat();
-        edge.convertTo(draw, CV_8U);
-//
-        while(draw.cols()>1000) { //TODO: make this better
-            Imgproc.resize(draw, draw, new Size(draw.cols()*0.7,draw.rows()*0.7));
-        }
-//
-        Imgcodecs.imwrite("draw.jpg", draw);
-
-        Mat blur = new Mat();
-//        Imgproc.GaussianBlur(draw, blur, new Size(9, 9), 2, 2);
-        Imgproc.medianBlur(draw, blur, 1);
-        Imgcodecs.imwrite("blur.jpg", blur);
-
-
-        Mat circles = new Mat();
-
-        Imgproc.HoughCircles(blur, circles, CV_HOUGH_GRADIENT, 1, blur.rows() / 16,200, 100, 0, 0);
-
-        System.out.println(circles.size());
-
-//        TimeUnit.SECONDS.sleep(1);
-
-        for (int x = 0; x < circles.cols(); x++) {
-            double[] c = circles.get(0, x);
-            Point center = new Point(Math.round(c[0]), Math.round(c[1]));
-            // circle center
-            Imgproc.circle(blur, center, 1, new Scalar(0,100,100), 3, 8, 0 );
-            // circle outline
-            int radius = (int) Math.round(c[2]);
-            Imgproc.circle(blur, center, radius, new Scalar(255,0,255), 3, 8, 0 );
-        }
-        HighGui.imshow("detected circles", blur);
-        HighGui.waitKey();
-
-//        System.exit(0);
-
-
-        return list;
-
     }
 
 }
