e796c42d3746dd7070ed29289b07cb0db6e66293
Sawyer <sawyercoletang@gmail.com>
2021-01-09 20:01:12 -0500

    fixed openCV dependency issue

--- src/main/java/openCV/CoinFinder.java
+++ src/main/java/openCV/CoinFinder.java
@@ -1,14 +1,30 @@
 package openCV;
 
 import org.opencv.core.Mat;
+import nu.pattern.OpenCV;
+import org.opencv.imgcodecs.Imgcodecs;
 
 import java.util.ArrayList;
 
+
+
 public class CoinFinder {
 
-    public ArrayList<Integer> findCoins(){
+    public static void main(String[] args){ //TODO: for debugging
 
+        OpenCV.loadShared();
+
+        String file = "src/main/resources/image2.jpg";
+        Mat src = Imgcodecs.imread(file, 0);
+        Mat dst = src;
+
+    }
+
+    public static ArrayList<Integer> findCoins(Mat mat){
         ArrayList<Integer> list = new ArrayList<Integer>();
+
+
+
 
         return list;
 
